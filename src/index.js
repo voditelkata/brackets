6961e99ad29816ed819fecee6e893a800bc2ab3f
voditelkatka <voditelkatka@gmail.com>
2021-03-07 17:44:15 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-module.exports = function check(str, b) {
-    let count = 0;
-    for (i = 0; i < str.length; i++) {
-        for (let j = 0; j < b.length; j++) {
-            if (str[i] == b[j][0]) {
-                count++;
-        }
-        if (str[i] == b[j][1]) {
-            count--;
-        } 
-        if (count < 0) {
-            return false;
-        } 
-    }
-}
-if (count != 0) {return false} else {return true};
-}
-'[(])'
-[['(', ')'], ['[', ']']];
+module.exports = function check(arr, bracketsConfig) {
+
+    let strArr  = bracketsConfig;
+    let open = [];
+    let close = [];
+    let stack = [];
+
+     for (var i = 0; i < strArr.length; i++){
+          open.push(strArr[i][0]);
+          close.push(strArr[i][1])
+     };
+  
+       for (let j = 0; j<arr.length; j++){
+           if ( (open.includes(arr[j]) && !close.includes(arr[j]) ) 
+            || (open.includes(arr[j]) && close.includes(arr[j]) && arr[j] !== stack[stack.length-1]) ) {
+            stack.push(arr[j])
+           }
+          
+          else{
+              if(close.indexOf(arr[j]) === open.indexOf( stack[stack.length-1] )){
+                  let p = stack.pop()
+              }else{
+                  return false
+              }
+          }
+      }
+          return stack.length === 0
+};
