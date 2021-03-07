89e1e53735d15b0c04f0add289be9559c7d25f2f
voditelkatka <voditelkatka@gmail.com>
2021-03-07 14:13:00 +0300

    add: help me

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, b) {
+    let count = 0;
+    for (i = 0; i < str.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            if (str[i] == b[j][0]) {
+                count++;
+        }
+        if (str[i] == b[j][1]) {
+            count--;
+        } 
+        if (count < 0) {
+            return false;
+        } 
+    }
 }
+if (count != 0) {return false} else {return true};
+}
+'[(])'
+[['(', ')'], ['[', ']']];
